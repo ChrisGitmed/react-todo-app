45a5cef59796287b186adff0c6326357569dd173
Chris Gitmed <cgitmed@gmail.com>
2021-02-21 08:50:03 +0000

    Created schema and test data for the database.

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+set client_min_messages to warning;
+
+-- DANGER: this is NOT how to do it in the real world.
+-- `drop schema` INSTANTLY ERASES EVERYTHING.
+drop schema "public" cascade;
+
+create schema "public";
+
+create table "todos" (
+  "todoId" serial not null,
+  "task" text not null,
+  "isCompleted" bool
+)
