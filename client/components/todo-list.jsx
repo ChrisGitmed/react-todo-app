b6eaa9d8e0ab2a77c9bb6e3a298a598cb5706a73
Chris Gitmed <cgitmed@gmail.com>
2021-02-21 23:52:12 +0000

    Added an effect hook to the TodoList which fetches a get request to the server.

--- client/components/todo-list.jsx
+++ client/components/todo-list.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function TodoList() {
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then(res => {
+        // eslint-disable-next-line no-console
+        console.log('res: ', res);
+      });
+  });
+
   return (
     <div className="todo-list-container">
       <ul className="todo-list"></ul>
