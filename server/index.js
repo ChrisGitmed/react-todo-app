a1aa8f0d24acd2c0c25921a174a1629dedfcea3b
Chris Gitmed <cgitmed@gmail.com>
2021-02-21 09:32:06 +0000

    Added a get route on the server.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/api/todos', (req, res, next) => {
+  res.status(200).send('it works!');
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
