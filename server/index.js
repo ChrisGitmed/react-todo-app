5bed96cb470c2ed660ecaeb39491c1225a1562a0
Chris Gitmed <cgitmed@gmail.com>
2021-02-21 09:39:29 +0000

    Expanded the get route to select all todos from the DB.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 require('dotenv/config');
 const express = require('express');
+const pg = require('pg');
 
 const app = express();
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.get('/api/todos', (req, res, next) => {
-  res.status(200).send('it works!');
+  const sql = `
+    select *
+      from todos
+  `;
+  db.query(sql)
+    .then(result => res.status(200).json(result.rows))
+    .catch(err => next(err));
 });
 
 app.listen(process.env.PORT, () => {
