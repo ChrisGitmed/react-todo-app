8a91f8515bdb5bd90b18635a5a3fb83e1b98a97e
Chris Gitmed <cgitmed@gmail.com>
2021-02-22 00:07:36 +0000

    Enhanced the TodoList to render a list item for each todo in the server's response.

--- client/components/todo-list.jsx
+++ client/components/todo-list.jsx
@@ -1,18 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function TodoList() {
-
+  const [todoList, setTodoList] = useState([]);
   useEffect(() => {
     fetch('/api/todos')
-      .then(res => {
-        // eslint-disable-next-line no-console
-        console.log('res: ', res);
+      .then(res => res.json())
+      .then(result => {
+        setTodoList(result);
       });
+  });
+
+  const listItems = todoList.map(todo => {
+    const { isCompleted, task, todoId } = todo;
+    return (
+      <li key={todoId}>
+        <div>
+          <p>{task}</p>
+          <span>{isCompleted}</span>
+        </div>
+      </li>
+    );
   });
 
   return (
     <div className="todo-list-container">
-      <ul className="todo-list"></ul>
+      <ul className="todo-list">
+        {listItems}
+      </ul>
     </div>
   );
 }
